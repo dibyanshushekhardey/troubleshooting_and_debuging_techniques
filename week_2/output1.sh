303209858dd76476f3f084c947cc332eef7c0934
dibyanshushekhardey <dibyanshushekhardey@gmail.com>
2020-07-30 17:02:51 +0530

    Added the output file for the thumbnail_generator.py file when there was ThreadPool written

---
+++ week_2/output1.sh
@@ -0,0 +1,9 @@
+$ time ./thumbnail_generator.py
+Traceback (most recent call last):
+  File "./thumbnail_generator.py", line 10, in <module>
+    import PIL
+ModuleNotFoundError: No module named 'PIL'
+
+real    0m2.158s
+user    0m0.000s
+sys     0m0.000s
