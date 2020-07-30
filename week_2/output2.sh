31b9c1574cc9b5c13089bd6e556fc3d936498972
dibyanshushekhardey <dibyanshushekhardey@gmail.com>
2020-07-30 17:07:17 +0530

    Output file of the python script when ThreadPool was changed to ProcessPool

---
+++ week_2/output2.sh
@@ -0,0 +1,9 @@
+$ time ./thumbnail_generator.py
+Traceback (most recent call last):
+  File "./thumbnail_generator.py", line 10, in <module>
+    import PIL
+ModuleNotFoundError: No module named 'PIL'
+
+real    0m0.216s
+user    0m0.000s
+sys     0m0.030s
